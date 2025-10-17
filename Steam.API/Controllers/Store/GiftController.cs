a41215dcf88ec44f5b662b07fd22c2ba97fb1d1a
Sabit Jafarov <ceferovsabit999@gmail.com>
2025-10-17 17:27:13 +0400

    Updated Gift section

--- Steam.API/Controllers/Store/GiftController.cs
+++ Steam.API/Controllers/Store/GiftController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Steam.Application.DTOs.Pagination;
 using Steam.Application.DTOs.Store.Gift;
 using Steam.Application.Services.Store.Interfaces;
+using Steam.Domain.Entities.Identity;
 using System.Security.Claims;
 
 namespace Steam.API.Controllers.Store
@@ -12,19 +14,45 @@ namespace Steam.API.Controllers.Store
     public class GiftController : ControllerBase
     {
         private readonly IGiftService _service;
+        private readonly UserManager<AppUser> _userManager;
 
-        public GiftController(IGiftService service)
+        public GiftController(IGiftService service, UserManager<AppUser> userManager)
         {
             _service = service;
+            _userManager = userManager;
         }
 
         [HttpPost("send")]
         [Authorize]
         [ProducesResponseType(typeof(GiftReturnDto), 201)]
         [ProducesResponseType(400)]
-        public async Task<ActionResult<GiftReturnDto>> SendGift([FromBody] GiftCreateDto dto)
+        [ProducesResponseType(404)]
+        public async Task<ActionResult<GiftReturnDto>> SendGift([FromBody] GiftCreateForUserDto dto)
         {
-            var result = await _service.SendGiftAsync(dto);
+            // Göndərənin ID-sini token-dən götürürük
+            var senderId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (string.IsNullOrEmpty(senderId))
+            {
+                return Unauthorized();
+            }
+
+            // Hədiyyə alacaq istifadəçini istifadəçi adına görə tapırıq
+            var receiverUser = await _userManager.FindByNameAsync(dto.ReceiverUsername);
+            if (receiverUser == null)
+            {
+                return NotFound($"User with username '{dto.ReceiverUsername}' not found.");
+            }
+
+            // Servisin gözlədiyi orijinal DTO-nu yaradırıq (ID-lər ilə)
+            var serviceDto = new GiftCreateDto
+            {
+                SenderId = senderId,
+                ReceiverId = receiverUser.Id,
+                ApplicationId = dto.ApplicationId
+            };
+
+            // Servisi çağırırıq
+            var result = await _service.SendGiftAsync(serviceDto);
             return CreatedAtAction(nameof(GetGiftById), new { id = result.Id }, result);
         }
 
---
+++ Steam.Application/DTOs/Store/Gift/GiftCreateForUserDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Steam.Application.DTOs.Store.Gift
+{
+    public record GiftCreateForUserDto
+    {
+        public string ReceiverUsername { get; init; } = default!;
+        public int ApplicationId { get; init; }
+    }
+}
